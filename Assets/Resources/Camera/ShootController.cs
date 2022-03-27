97e33c764f704e3ab12cbf65454fdffda4f65b02
htakahisa <ronethousand@gmail.com>
2022-03-27 12:29:32 +0900

    init

---
+++ Assets/Resources/Camera/ShootController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    public float shotSpeed;
+    public int shotCount = 30;
+
+
+    private float shotInterval = 0.1f;
+    private float shotDeltatime = 999f;
+
+    void Update()
+    {
+
+        shotDeltatime += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (shotDeltatime < shotInterval)
+            {
+                return;
+            }
+            shotDeltatime = 0;
+
+            shotCount -= 1;
+
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0));
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            bulletRb.AddForce(transform.forward * shotSpeed);
+
+            //éÀåÇÇ≥ÇÍÇƒÇ©ÇÁ3ïbå„Ç…èeíeÇÃÉIÉuÉWÉFÉNÉgÇîjâÛÇ∑ÇÈ.
+
+            Destroy(bullet, 3.0f);
+            
+
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            shotCount = 30;
+
+        }
+
+    }
+}
