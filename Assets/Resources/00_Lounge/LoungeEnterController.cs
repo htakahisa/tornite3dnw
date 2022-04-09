adc734b76ebd1ba4781e7a3999c95561ece240a3
htakahisa <ronethousand@gmail.com>
2022-04-09 18:42:57 +0900

    add lounge

---
+++ Assets/Resources/00_Lounge/LoungeEnterController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LoungeEnterController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    PhotonView uiSync;
+
+    [SerializeField]
+    Text userCountText;
+
+    [SerializeField]
+    PublicProperty pp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickEnter()
+    {
+        uiSync.RPC("addUserCount", RpcTarget.All);
+
+        userCountText.text = "USER COUNT : " +  pp.getUserCount().ToString();
+    }
+}
---
+++ Assets/Resources/PublicProperty.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PublicProperty : MonoBehaviourPun
+{
+
+    // ユーザー数
+    private int userCount;
+
+
+    [PunRPC]//RPCを使って呼べようにするにはこの属性をつける(これが無いと呼べない)
+    void setUserCount(int i)
+    {
+        this.userCount = i;
+    }
+
+    [PunRPC]//RPCを使って呼べようにするにはこの属性をつける(これが無いと呼べない)
+    void addUserCount()
+    {
+        this.userCount = this.userCount + 1;
+    }
+
+    public int getUserCount()
+    {
+        return this.userCount;
+    }
+}
