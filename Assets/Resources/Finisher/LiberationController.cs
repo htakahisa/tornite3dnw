873156ca0630cd8bea7b0f2c177a8add0dc3b35d
tk <ronethousand@gmail.com>
2025-01-03 20:43:27 +0900

    Talorant Version 1.1.4
    ・blacknoir finisher 追加

--- Assets/Resources/Finisher/LiberationController.cs
+++ Assets/Resources/Finisher/LiberationController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class LiberationController : MonoBehaviour
 {
+    [SerializeField]
     private float delay = 3.5f; // 待機時間（秒）
-    public float moveSpeed = 3.0f; // 移動スピード（変更可能）
+    [SerializeField]
+    private float moveSpeed = 3.0f; // 移動スピード（変更可能）
 
     public Vector3 moveOffset = new Vector3(0, 500.0f, 0); // 移動量
     private Vector3 startPosition;
