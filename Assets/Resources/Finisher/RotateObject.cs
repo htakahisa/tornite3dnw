a5308d0d58c01f6aeb77d2750ba449f155e0ae26
tk <ronethousand@gmail.com>
2025-01-03 13:23:26 +0900

    Talorant version 1.0.9.6
    ・snow finisher 追加

---
+++ Assets/Resources/Finisher/RotateObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0, 50, 0); // Y軸を中心に回転させる速度
+
+    void Update()
+    {
+        // 毎フレーム回転させる
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
--- Assets/Resources/Skin/SkinIF.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface SkinIF
-{
-    public string getSkinName();
-}
